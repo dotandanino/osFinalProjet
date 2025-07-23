b625037eda43fb779359e0ecfd1b8e99ff406c4b
dotandanino <dotandanino@gmail.com>
2025-07-23 23:18:53 +0300

    initial commit

---
+++ question8/client.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+#define SERVER_IP "127.0.0.1"
+using namespace std;
+
+int main(){
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    
+    
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        cerr << "Socket creation error" << endl;
+        return -1;
+    }
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    
+    
+    if(inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
+        cerr << "Invalid address / Address not supported" << endl;
+        return -1;
+    }
+    
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        cerr << "Connection Failed" << endl;
+        return -1;
+    }
+    int n =0 ;
+    int src,dest,w;
+    for(;;){
+        cout<<"enter number of vertices if you want to exit enter -1 if you want random graph enter 0"<<endl;
+        cin>>n;
+        if(n == 0) {
+            cout << "Random graph selected." << endl;
+            cout<< "Enter number of vertices, edges, random seed, min weight , max weight: ";
+            int numOfVertex, numOfEdges, RandomSeed, minWeight, maxWeight;
+            cin >> numOfVertex >> numOfEdges >> RandomSeed >> minWeight >> maxWeight;
+            int arr[6] = {n, numOfVertex, numOfEdges, RandomSeed, minWeight, maxWeight};
+            send(sock, arr, sizeof(arr), 0);
+        }
+        else if(n==-1){
+            close(sock);
+            cout << "Connection closed." << endl;
+            break;
+        }
+        else if(n<-1){
+            cout<<"n must be positive number"<<endl;
+            continue;
+        }else{
+           int arr[n*n+1];//n*n for the neighbor matrix and extra place for the size
+            arr[0]=n;
+            for(int i=1;i<n*n+1;i++){
+                arr[i]=-1; // initialize the matrix with -1
+            }
+            cout<<"now you will need to enter the neighbor matrix if you want to stop enter 0 for both src and dest"<<endl;
+            for(;;){
+                cout<<"enter src "<<endl;
+                cin>>src;
+                cout<<"enter dst"<<endl;
+                cin>>dest;
+                if(dest==src && dest==0){
+                    break;
+                }
+                if(dest<0 || dest>=n || src<0 || src>=n || src==dest){
+                    cout<<"illegal arguments src,dest should different numbers be between 0 to n-1"<<endl;
+                    continue;
+                }
+                cout<<"enter weight"<<endl;
+                cin>>w;
+                arr[src*n+dest+1]=w;//src*n + dest to change from matrix to arr +1 is because of the size
+            }
+            send(sock, &arr,(n*n + 1)*sizeof(int), 0);
+            cout << "Graph sent to server." << endl;  
+        }
+        char buffer[4096] = {0};
+        int bytesReceived = recv(sock, buffer, sizeof(buffer), 0);
+        if (bytesReceived > 0) {
+            std::string received(buffer, bytesReceived);
+            cout << received << endl;
+        }
+    }
+
+
+}
---
+++ question8/server.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <poll.h>
+#include <unistd.h>
+#include "../question7/Graph.hpp"
+#include "../question7/algoFactory.hpp"
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <queue>
+#include <condition_variable>
+
+#define PORT 8080
+#define BUFFERSIZE 4096
+using namespace std;
+#define N 5
+std::mutex mtx; // Mutex for thread safety
+std::condition_variable cv;
+
+struct toqueue {
+    int fd;
+    Graph g;
+};
+
+std::queue<int> q; // Queue to hold file descriptors
+
+std::unique_ptr<GraphAlgorithm> algo1 = createAlgorithm(static_cast<AlgorithmType>(1));
+std::unique_ptr<GraphAlgorithm> algo2 = createAlgorithm(static_cast<AlgorithmType>(2));
+std::unique_ptr<GraphAlgorithm> algo3 = createAlgorithm(static_cast<AlgorithmType>(3));
+std::unique_ptr<GraphAlgorithm> algo4 = createAlgorithm(static_cast<AlgorithmType>(4));
+
+
+Graph createRandomGraph(int numOfVertex, int numOfEdges, int RandomSeed,int minWeight, int maxWeight) {
+    unsigned long maxEdge = (numOfVertex -1)*numOfVertex;
+    if((unsigned long)numOfEdges>maxEdge){
+        std::cerr<<"E"<<numOfEdges<<" maxEdge "<<maxEdge<<std::endl; 
+        std::cerr<<"ypu enter too many edges"<<std::endl;
+        exit(1);
+    }
+    int edgesAdded=0;
+    Graph g(numOfVertex);
+    srand(RandomSeed);
+    while(edgesAdded<numOfEdges){
+        int src=rand()%numOfVertex;
+        int dest =rand()%numOfVertex;
+        int weight = rand() % (maxWeight - minWeight + 1) + minWeight;
+        try{
+            g.addDirectedEdge(src,dest,weight);
+            edgesAdded++;
+        }catch(const std::exception& e){
+            //nothing to do here just wait for another graph
+        }
+    }
+    g.printGraph();
+    return g;
+}
+
+void clientHandle(){
+    while(true) {
+        int fd = -1;
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, []{ return !q.empty(); }); // מחכה עד שיש משהו בתור
+            fd = q.front();
+            q.pop();
+        }
+        if (fd == -1) {
+            continue;
+        }
+        int buffer[BUFFERSIZE / sizeof(int)];
+        int byteRecv = recv(fd,buffer,BUFFERSIZE,0);
+        if(byteRecv==0){
+            cout << "Client disconnected." << endl;
+            close(fd);
+            break;
+        }
+        int n=buffer[0];
+        if(n == 0) {
+            cout << "Random graph selected." << endl;
+            int numOfVertex = buffer[1];
+            int numOfEdges = buffer[2];
+            int RandomSeed = buffer[3];
+            int minWeight = buffer[4];
+            int maxWeight = buffer[5];
+            if (minWeight > maxWeight) {
+                string e = "Error: minWeight cannot be greater than maxWeight";
+                send(fd, e.c_str(), e.size(), 0);
+                continue;
+            }
+            cout << "Creating random graph with " << numOfVertex << " vertices, " << numOfEdges << " edges, and random seed " << RandomSeed << endl;
+            Graph g = createRandomGraph(numOfVertex, numOfEdges, RandomSeed,minWeight, maxWeight);
+            cout<<"Before MaxFlow"<<endl;
+            std::string result = algo1->execute(g);
+            cout<<"Before MST"<<endl;
+            result += "\n" + algo2->execute(g);
+            cout<<"Before PathCover"<<endl;
+            result += "\n" + algo3->execute(g);
+            cout<<"Before SCC"<<endl;
+            result += "\n" + algo4->execute(g);
+            cout << "Sending results to client..." << endl;
+            send(fd, result.c_str(), result.size(), 0);
+            continue;
+        }
+        vector<vector<int>> neighborsMatrix(n, vector<int>(n, -1));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                neighborsMatrix[i][j]=buffer[i*n+j+1];
+            }
+        }
+        Graph g(n,neighborsMatrix);
+        cout<<"Before MaxFlow"<<endl;
+        std::string result = algo1->execute(g);
+        cout<<"Before MST"<<endl;
+        result += "\n" + algo2->execute(g);
+        cout<<"Before PathCover"<<endl;
+        result += "\n" + algo3->execute(g);
+        cout<<"Before SCC"<<endl;
+        result += "\n" + algo4->execute(g);
+        cout << "Sending results to client..." << endl;
+        send(fd, result.c_str(), result.size(), 0);
+    }
+}
+
+int main(){
+    int server_fd;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    std::thread threads[4];
+    for (int i = 0; i < 4; ++i) {
+        threads[i] = std::thread(clientHandle); // Create threads for handling clients
+    }
+
+
+    // create the socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // set socket options to allow reuse of the address and port
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    
+    // connect the socket to the address and port
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // create a queue with 10 places for the socket to listen for incoming connections
+    if (listen(server_fd, 10) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+    int fd_count = 1;
+    int fd_capacity = 1;
+    // a vector to hold the file descriptors for polling
+    vector<pollfd> fds(fd_capacity);
+    fds[0].fd = server_fd;
+    fds[0].events = POLLIN;
+
+    int buffer[BUFFERSIZE / sizeof(int)];
+    for(;;){
+        int poll_count = poll(fds.data(), fd_count, -1);
+        if (poll_count < 0) {
+            perror("poll failed");
+            exit(EXIT_FAILURE);
+        }
+        for(int i=0;i<fd_count;i++){
+            if(fds[i].revents & POLLIN){
+                if(fds[i].fd == server_fd){
+                    int new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+                    fds.push_back({new_socket, POLLIN});
+                    fd_count++;
+                }
+                else{
+                    {
+                        std::lock_guard<std::mutex> lock(mtx);
+                        q.push(fds[i].fd);
+                        cv.notify_one();
+                    }
+                }
+            }
+        }
+    }
+}
