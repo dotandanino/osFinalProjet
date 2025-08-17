89d131ce4c3115f0a758bb382b6cac65e580836b
dotandanino <dotandanino@gmail.com>
2025-08-17 13:06:46 +0300

    last commit

--- question8/server.cpp
+++ question8/server.cpp
@@ -17,21 +17,13 @@
 #define PORT 8080
 #define BUFFERSIZE 4096
 using namespace std;
-#define N 5
-std::mutex mtx; // Mutex for thread safety
+int server_fd;
+std::mutex mtx;
 std::condition_variable cv;
-int fd_count = 2;
-// a vector to hold the file descriptors for polling
-vector<pollfd> fds(fd_count);
+bool hasLeader = false;
+bool server_running = true;
 
-std::queue<int> q; // Queue to hold file descriptors
-
-std::unique_ptr<GraphAlgorithm> algo1 = createAlgorithm(static_cast<AlgorithmType>(1));
-std::unique_ptr<GraphAlgorithm> algo2 = createAlgorithm(static_cast<AlgorithmType>(2));
-std::unique_ptr<GraphAlgorithm> algo3 = createAlgorithm(static_cast<AlgorithmType>(3));
-std::unique_ptr<GraphAlgorithm> algo4 = createAlgorithm(static_cast<AlgorithmType>(4));
-
-bool server_running = true; // Flag to control server running state
+vector<pollfd> fds; 
 
 Graph createRandomGraph(int numOfVertex, int numOfEdges, int RandomSeed,int minWeight, int maxWeight) {
     unsigned long maxEdge = (numOfVertex -1)*numOfVertex;
@@ -57,82 +49,133 @@ Graph createRandomGraph(int numOfVertex, int numOfEdges, int RandomSeed,int minW
     return g;
 }
 
-void clientHandle(){
-    while(server_running) {
-        int fd = -1;
-        {
-            std::unique_lock<std::mutex> lock(mtx);
-            cv.wait(lock, []{ return !q.empty() || !server_running; });
-            if(!q.empty()) {
-                fd = q.front();
-                q.pop();
-            }
+void clientHandle(int fd){
+    cout<<"connect to "<<fd<<endl;
+    std::unique_ptr<GraphAlgorithm> algo1 = createAlgorithm(static_cast<AlgorithmType>(1));
+    std::unique_ptr<GraphAlgorithm> algo2 = createAlgorithm(static_cast<AlgorithmType>(2));
+    std::unique_ptr<GraphAlgorithm> algo3 = createAlgorithm(static_cast<AlgorithmType>(3));
+    std::unique_ptr<GraphAlgorithm> algo4 = createAlgorithm(static_cast<AlgorithmType>(4));
+    if(!server_running) {
+        return; // Exit if server is not running and no file descriptor is available
+    }
+    int buffer[BUFFERSIZE / sizeof(int)];
+    int byteRecv = recv(fd,buffer,BUFFERSIZE,0);
+    if(byteRecv==0){
+        close(fd);
+        auto it = std::remove_if(fds.begin(), fds.end(), [fd](const pollfd& p) {
+            return p.fd == fd;
+        });
+        fds.erase(it, fds.end());
+        return;
+    }
+    int n=buffer[0];
+    if(n == 0) {
+        cout << "Random graph selected." << endl;
+        int numOfVertex = buffer[1];
+        int numOfEdges = buffer[2];
+        int RandomSeed = buffer[3];
+        int minWeight = buffer[4];
+        int maxWeight = buffer[5];
+        if (minWeight > maxWeight) {
+            string e = "Error: minWeight cannot be greater than maxWeight";
+            send(fd, e.c_str(), e.size(), 0);
+                return;
         }
-        if(!server_running && fd == -1) {
-            break; // Exit if server is not running and no file descriptor is available
-        }
-        if (fd == -1) {
-            continue;
-        }
-        int buffer[BUFFERSIZE / sizeof(int)];
-        int byteRecv = recv(fd,buffer,BUFFERSIZE,0);
-        if(byteRecv==0){
-            close(fd);
-            auto it = std::remove_if(fds.begin(), fds.end(), [fd](const pollfd& p) {
-                return p.fd == fd;
-            });
-            fds.erase(it, fds.end());
-            break;
-        }
-        int n=buffer[0];
-        if(n == 0) {
-            cout << "Random graph selected." << endl;
-            int numOfVertex = buffer[1];
-            int numOfEdges = buffer[2];
-            int RandomSeed = buffer[3];
-            int minWeight = buffer[4];
-            int maxWeight = buffer[5];
-            if (minWeight > maxWeight) {
-                string e = "Error: minWeight cannot be greater than maxWeight";
-                send(fd, e.c_str(), e.size(), 0);
-                continue;
-            }
-            cout << "Creating random graph with " << numOfVertex << " vertices, " << numOfEdges << " edges, and random seed " << RandomSeed << endl;
-            Graph g = createRandomGraph(numOfVertex, numOfEdges, RandomSeed,minWeight, maxWeight);
-            std::string result = algo1->execute(g);
-            result += "\n" + algo2->execute(g);
-            result += "\n" + algo3->execute(g);
-            result += "\n" + algo4->execute(g);
-            cout << "Sending results to client..." << endl;
-            send(fd, result.c_str(), result.size(), 0);
-            continue;
-        }
-        vector<vector<int>> neighborsMatrix(n, vector<int>(n, -1));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                neighborsMatrix[i][j]=buffer[i*n+j+1];
-            }
-        }
-        Graph g(n,neighborsMatrix);
+        cout << "Creating random graph with " << numOfVertex << " vertices, " << numOfEdges << " edges, and random seed " << RandomSeed << endl;
+        Graph g = createRandomGraph(numOfVertex, numOfEdges, RandomSeed,minWeight, maxWeight);
         std::string result = algo1->execute(g);
         result += "\n" + algo2->execute(g);
         result += "\n" + algo3->execute(g);
         result += "\n" + algo4->execute(g);
         cout << "Sending results to client..." << endl;
         send(fd, result.c_str(), result.size(), 0);
+        return;
     }
+    vector<vector<int>> neighborsMatrix(n, vector<int>(n, -1));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            neighborsMatrix[i][j]=buffer[i*n+j+1];
+        }
+    }
+    Graph g(n,neighborsMatrix);
+    std::string result = algo1->execute(g);
+    result += "\n" + algo2->execute(g);
+    result += "\n" + algo3->execute(g);
+    result += "\n" + algo4->execute(g);
+    cout << "Sending results to client..." << endl;
+    send(fd, result.c_str(), result.size(), 0);
 }
 
+void worker(int id) {
+    while (server_running) {
+        unique_lock<mutex> lock(mtx);
+
+        cv.wait(lock, []{ return !hasLeader || !server_running; });//wait untill there is no leader
+        if (!server_running) break;
+
+        //become the leader
+        hasLeader = true;
+        lock.unlock();
+
+        // Leader waits for an event
+        int poll_count = poll(fds.data(), fds.size(), 5000);
+
+        // Once I found an event, I release the role
+        lock.lock();
+        hasLeader = false;
+        cv.notify_one(); // Wake up another thread to become Leader
+        lock.unlock();
+
+        if (poll_count < 0) {
+            perror("poll failed");
+            break;
+        }
+
+        // I am already a Follower, so I handle the event myself
+        for (size_t i = 0; i < fds.size(); i++) {
+            if(!server_running) {
+                return; // Exit if server is not running
+            }
+            if (fds[i].revents & POLLIN) {
+                if (fds[i].fd == fds[0].fd) {
+                    // Event on server_fd -> accept
+                    struct sockaddr_in address;
+                    socklen_t addrlen = sizeof(address);
+                    int new_socket = accept(fds[0].fd, (struct sockaddr*)&address, &addrlen);
+                     if (errno == EBADF || errno == EINVAL) {// Check if the socket is closed
+                        cout << "Server socket closed" << endl;
+                        return;
+                    }
+                    if (new_socket >= 0) {
+                        cout << "New client accepted: " << new_socket << endl;
+                        lock_guard<mutex> l(mtx);
+                        fds.push_back({new_socket, POLLIN, 0});
+                    }
+                } else if (fds[i].fd == STDIN_FILENO) {
+                    string input;
+                    getline(cin, input);
+                    if (input == "exit") {
+                        lock_guard<mutex> l(mtx);
+                        server_running = false;
+                        shutdown(server_fd, SHUT_RDWR);
+                        for (size_t j = 2; j < fds.size(); j++) {
+                            close(fds[j].fd); // Close all client sockets
+                        }
+                        cv.notify_all();
+                        cout << "Server shutting down..." << endl;
+                        return;
+                    }
+                } else {
+                    clientHandle(fds[i].fd);
+                }
+            }
+        }
+    }
+}
 int main(){
-    int server_fd;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    std::thread threads[4];
-    for (int i = 0; i < 4; ++i) {
-        threads[i] = std::thread(clientHandle); // Create threads for handling clients
-    }
-
 
     // create the socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -161,55 +204,24 @@ int main(){
         perror("listen failed");
         exit(EXIT_FAILURE);
     }
-    fds[0].fd = server_fd;
-    fds[0].events = POLLIN;
-    fds[1].fd = STDIN_FILENO; // Add stdin to the poll list
-    fds[1].events = POLLIN;
 
-    for(;;){
-        int poll_count = poll(fds.data(), fd_count, -1);
-        if (poll_count < 0) {
-            perror("poll failed");
-            exit(EXIT_FAILURE);
-        }
-        for(int i=0;i<fd_count;i++){
-            if(fds[i].revents & POLLIN){
-                if(fds[i].fd == server_fd){
-                    int new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-                    fds.push_back({new_socket, POLLIN});
-                    fd_count++;
-                }else if(fds[i].fd == STDIN_FILENO){
-                    // Handle input from stdin
-                    string input;
-                    getline(cin, input);
-                    if(input == "exit") {
-                        server_running = false;
-                        for(int j=2; j<fd_count; j++) {
-                            close(fds[j].fd); // Close all client sockets
-                        }
-                        close(server_fd); // Close the server socket
-                        server_running = false; // Set the server running flag to false
-                        cv.notify_all(); // Notify all waiting threads to exit
-                        cout<<"Server shutting down..."<<endl;
-                        for(int i=0;i<4;i++){
-                            threads[i].join(); // Wait for all threads to finish
-                        }
-                        for(int i=0;i<fd_count;i++){
-                            if(fds[i].fd != -1) {
-                                close(fds[i].fd); // Close all file descriptors
-                            }
-                        }
-                        exit(0);
-                    }
-                }
-                else{
-                    {
-                        std::lock_guard<std::mutex> lock(mtx);
-                        q.push(fds[i].fd);
-                        cv.notify_one();
-                    }
-                }
-            }
-        }
+    fds.push_back({server_fd, POLLIN, 0});
+    fds.push_back({STDIN_FILENO, POLLIN, 0});
+
+    const int N = 4;
+    vector<thread> threads;
+    for (int i = 0; i < N; i++) {
+        threads.emplace_back(worker, i+1);
     }
+
+    {
+        lock_guard<mutex> lock(mtx);
+        hasLeader = false;
+        cv.notify_one();
+    }
+
+    for (auto &t : threads) t.join();
+
+    close(server_fd);
+    return 0;
 }
